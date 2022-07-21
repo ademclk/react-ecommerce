173099d8c43277af18dfe05a507161d375bd15dc
ademclk <ademonurcelik@gmail.com>
2022-07-21 20:38:51 +0300

    Added functionality of getting single product details.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, createProduct } = require('../controllers/productController')
+const { getProducts, createProduct, getSingleProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
+
 router.route('/product/new').post(createProduct);
 
 module.exports = router;
