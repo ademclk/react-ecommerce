61868c70ec0739b1cdece0b3d917c03d763640a8
ademclk <ademonurcelik@gmail.com>
2022-07-21 21:32:20 +0300

    Admin routes and added functionality of updating product data.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, createProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, createProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/product/new').post(createProduct);
+router.route('/admin/product/new').post(createProduct);
+
+router.route('/admin/product/:id').put(updateProduct);
 
 module.exports = router;
