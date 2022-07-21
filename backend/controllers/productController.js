16d5d723efcebf58c21e3093210730e1a388abd4
ademclk <ademonurcelik@gmail.com>
2022-07-21 22:12:44 +0300

    Added the functionality of deleting a product.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -64,5 +64,24 @@ exports.updateProduct = async (req, res, next) => {
         success: true,
         product
     })
+}
 
+// Delete a product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found :('
+        })
+    }
+
+    await Product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product deleted successfully.'
+    })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, createProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
+const { getProducts, 
+        createProduct, 
+        getSingleProduct, 
+        updateProduct, 
+        deleteProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(createProduct);
 
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id')
+    .put(updateProduct)
+    .delete(deleteProduct);
 
 module.exports = router;
