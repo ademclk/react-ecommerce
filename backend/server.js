8ae35c25b1fdbe9906aaf48ac0ba0f1b91441cd0
ademclk <ademonurcelik@gmail.com>
2022-07-22 13:14:22 +0300

    Handled the Unhandled Promise Rejection error when database connection doesn't make correctly.

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ dotenv.config({ path: 'backend/config/config.env'})
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${err.message}`);
+    console.log('Unhandled promise rejection. Server shutting down...');
+    server.close(() => {
+        process.exit(1);
+    })
+})
