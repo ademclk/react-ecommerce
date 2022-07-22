c54dc72328c14ccd6147d2b0989d6abfd619afad
ademclk <ademonurcelik@gmail.com>
2022-07-22 19:56:01 +0300

    Handled mongoose validation error.

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -23,6 +23,12 @@ module.exports = (err, req, res, next) => {
             error = new ErrorHandler(message, 400)
         }
 
+        // Mongoose validation error
+        if (err.name === "ValidationError") {
+            const message = Object.values(err.errors).map(value => value.message);
+            error = new ErrorHandler(message, 400);
+        }
+
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
