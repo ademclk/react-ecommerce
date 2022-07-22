6651845add4c1cb929ab9732af8e8c606cec4a73
ademclk <ademonurcelik@gmail.com>
2022-07-22 23:56:19 +0300

    User password reset token and it's expire date.

--- backend/models/user.js
+++ backend/models/user.js
@@ -37,7 +37,9 @@ const userSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
 
 })
 
