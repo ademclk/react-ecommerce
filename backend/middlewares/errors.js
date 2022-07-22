19b7a06e3fabafefc5903b8569e6b925f5ae76de
ademclk <ademonurcelik@gmail.com>
2022-07-22 08:44:06 +0300

    Development and production error handling seperated.

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,10 +2,24 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500; // 500 is the default status code (internal server error)
-    err.message = err.message || 'Internal server error. Something went wrong';
-    
-    res.status(err.statusCode).json({
-        success: false,
-        error: err.stack
-    })
+
+    if (process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.errMessage,
+            stack: err.stack
+        })
+    }
+
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+        let error = { ...err }
+
+        error.message = err.message
+
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        })
+    }
 }
