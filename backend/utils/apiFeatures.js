c1744a91949e3ec590ba45e249b81f5ea6a9bce3
ademclk <ademonurcelik@gmail.com>
2022-07-22 22:53:11 +0300

    Filtering added for product.

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,3 +1,5 @@
+const { remove } = require("../models/product");
+
 class APIFeatures {
     constructor(query, queryStr){
         this.query = query;
@@ -17,6 +19,23 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword })
         return this; 
     }
+
+    filter () {
+        const queryCopy = {...this.queryStr};
+
+        console.log(queryCopy)
+
+        // Remove the fields from the query string
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        // Advanced filtering
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
 }
 
-module.exports = APIFeatures;
+module.exports = APIFeatures; 
