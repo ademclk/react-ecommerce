f756e6ee1e6951b26e00cf5787fa2be124bd325c
ademclk <ademonurcelik@gmail.com>
2022-07-24 18:28:12 +0300

    Added user for creating products.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,8 @@ const APIFeatures = require('../utils/apiFeatures');
 // Create a new product => /api/v1/admin/product/new
 exports.createProduct = catchAsyncError(async (req, res, next) => {
 
+    req.body.user = req.user.id;
+
     const product = await Product.create(req.body);
 
     res.status(201).json({
