20ade582bbf07f70a0d3428c0bf8777c7f3cc3a4
ademclk <ademonurcelik@gmail.com>
2022-07-25 23:08:49 +0300

    Get all orders route added for admin.

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder, getSingleOrder, getUserOrders } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, getUserOrders, getAllOrders } = require('../controllers/orderController');
 
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRole } = require('../middlewares/auth');
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
 
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, getUserOrders);
+
+router.route('/admin/orders').get(isAuthenticatedUser, authorizeRole('admin'), getAllOrders);
 
 module.exports = router;
