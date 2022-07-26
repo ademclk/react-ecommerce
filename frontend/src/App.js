4e428fb7b39aa953bda05924b123351312538b81
ademclk <ademonurcelik@gmail.com>
2022-07-26 22:22:20 +0300

    Header component added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/layout/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>React Ecommerce</h1>      
+      <Header />  
     </div>
   );
 }
