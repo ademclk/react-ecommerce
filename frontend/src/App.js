162a15668faeb8b9a794c718a7f5433541ac0f00
ademclk <ademonurcelik@gmail.com>
2022-07-26 22:27:06 +0300

    Footer component added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />  
+      <Footer />
     </div>
   );
 }
