5e8933414280775caeb809239b9f80f43c564ce9
ademclk <ademonurcelik@gmail.com>
2022-07-26 23:41:32 +0300

    Redux store created.

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({
+
+});
+
+let initialState = {};
+
+const middleware = [thunk];
+const store = configureStore({reducer}, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
