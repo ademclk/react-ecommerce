3d1bd230efbbf4aced0bbef8936b329c0c1d22bb
ademclk <ademonurcelik@gmail.com>
2022-07-27 00:02:53 +0300

    Added reducer for all products.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,10 @@ import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolk
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({
+import { productsReducer } from './reducers/productReducers';
 
+const reducer = combineReducers({
+    products: productsReducer
 });
 
 let initialState = {};
