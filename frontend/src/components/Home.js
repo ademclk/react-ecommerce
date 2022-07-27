132a488579b247f0a30e90601b51d600921d3f7d
ademclk <ademonurcelik@gmail.com>
2022-07-27 20:32:47 +0300

    Added react alert.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData'
 
 import { useDispatch, useSelector } from 'react-redux';
+import { useAlert } from 'react-alert';
 import { getProducts } from '../actions/productActions';
 
 import Product from './product/Product';
@@ -10,14 +11,21 @@ import Loader from './layout/Loader';
 
 const Home = () => {
 
+    const alert = useAlert();
     const dispatch = useDispatch();
 
     const { loading, products, error, productsCount } = useSelector(state => state.products);
 
     useEffect(() => {
-        dispatch(getProducts());
-    }, [])
 
+        if(error){
+            return alert.error(error);
+        }
+        
+        dispatch(getProducts());
+ 
+    }, [dispatch, alert, error])
+ 
     return (
         <Fragment>
             {loading ? <Loader /> : (
