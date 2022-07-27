c8b5fef661b1caa0bf94a34ce7894f7f2e67c355
ademclk <ademonurcelik@gmail.com>
2022-07-27 21:30:46 +0300

    Added reducer and action for product details.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
     ALL_PRODUCTS_SUCCESS,
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_FAILURE,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAILURE,
     CLEAR_ERRORS
 } from '../constants/productConstants';
 
@@ -22,6 +25,26 @@ export const getProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ALL_PRODUCTS_FAILURE,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const getProductDetails = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+        const { data } = await axios.get(`/api/v1/product/${id}`);
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data.product
+        });
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAILURE,
             payload: error.response.data.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,11 @@ import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolk
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productsReducer } from './reducers/productReducers';
+import { productsReducer, productDetailsReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
-    products: productsReducer
+    products: productsReducer,
+    productDetails: productDetailsReducer
 });
 
 let initialState = {};
